b1aeef1467c0827131f5105f9e1f5298dfa49896
Haegnim <haegnim@gmail.com>
2023-09-20 21:12:48 +0900

    Edit : add mission

--- src/components/game/MagicpowderReward.tsx
+++ src/components/game/MagicpowderReward.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { GameActionButton } from './GameUI.style';
-import { Water } from '../../assets/icons/GameIcon';
+import { MagicPowder, Water } from '../../assets/icons/GameIcon';
 import BottomSheet, {
     MissionButton,
     MissionIcon,
@@ -9,20 +9,18 @@ import BottomSheet, {
     MissionText,
     useBottomSheet,
 } from '../common/BottomSheet';
-import { useNavigate } from 'react-router-dom';
 // import { styled } from 'styled-components';
 
-const WaterReward = () => {
+const MagicpowderReward = () => {
     const { isOpen, setIsOpen, isShow, setIsShow, onClickShowHandler } = useBottomSheet(
         false,
         false
     );
-    const nav = useNavigate();
     return (
         <>
-            <GameActionButton onClick={onClickShowHandler} $color="#48B2FF">
-                <Water />
-                이슬
+            <GameActionButton onClick={onClickShowHandler} $color="#FF9548">
+                <MagicPowder />
+                마법가루
             </GameActionButton>
             {isOpen && (
                 <BottomSheet
@@ -30,35 +28,28 @@ const WaterReward = () => {
                     isShow={isShow}
                     setIsShow={setIsShow}
                     setIsOpen={setIsOpen}
-                    actionTitle={'이슬받기'}
+                    actionTitle={'마법가루 받기'}
                 >
-                    <MissionList $color={'#bbf3ff'}>
+                    <MissionList $color={'#FFE7C2'}>
                         <MissionItem>
                             <div className="mission-item-inner">
                                 <MissionIcon>d</MissionIcon>
-                                <MissionText>인기 급상승템 구경하기</MissionText>
-                                <MissionButton onClick={() => nav('/')}>바로 가기</MissionButton>
+                                <MissionText>공유하기</MissionText>
+                                <MissionButton>링크복사</MissionButton>
                             </div>
                         </MissionItem>
                         <MissionItem>
                             <div className="mission-item-inner">
                                 <MissionIcon>d</MissionIcon>
-                                <MissionText>오늘 뭐입지? 구경하기</MissionText>
-                                <MissionButton onClick={() => nav('/')}>바로 가기</MissionButton>
+                                <MissionText>매일 출석하기 (1/1)</MissionText>
+                                <MissionButton>출석하기</MissionButton>
                             </div>
                         </MissionItem>
                         <MissionItem>
                             <div className="mission-item-inner">
                                 <MissionIcon>d</MissionIcon>
-                                <MissionText>신상마켓 구경하기</MissionText>
-                                <MissionButton>바로 가기</MissionButton>
-                            </div>
-                        </MissionItem>
-                        <MissionItem>
-                            <div className="mission-item-inner">
-                                <MissionIcon>d</MissionIcon>
-                                <MissionText>리셀마켓 구경하기</MissionText>
-                                <MissionButton>바로 가기</MissionButton>
+                                <MissionText>상품 구매하기 (1/1)</MissionText>
+                                <MissionButton>둘러보기</MissionButton>
                             </div>
                         </MissionItem>
                     </MissionList>
@@ -68,4 +59,4 @@ const WaterReward = () => {
     );
 };
 
-export default WaterReward;
+export default MagicpowderReward;
