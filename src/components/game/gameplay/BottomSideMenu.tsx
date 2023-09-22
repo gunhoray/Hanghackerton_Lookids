650429c53cba2d3b204e6b35ecc13fe7661141df
Haegnim <haegnim@gmail.com>
2023-09-22 21:04:25 +0900

    Feat : add speech bubble

--- src/components/game/gameplay/BottomSideMenu.tsx
+++ src/components/game/gameplay/BottomSideMenu.tsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../../redux/config/configStore';
 import { useMutation, useQueryClient } from 'react-query';
 import { itemtypeProps, usingItem } from '../../../apis/fairy';
+import useSpeech from '../../../hooks/useSpeech';
 
 const BottomsideMenu = styled.div`
     display: flex;
@@ -49,6 +50,10 @@ const ButtonBox = styled.div`
     }
 `;
 
+const heartSpeech = ['저도 많이 사랑해요', '우와앙', '좋아요!'];
+const magicPowderSpeech = ['자라나라 쑥쑥'];
+const dewSpeech = ['감사합니다!', '마침 목이 말랐어요', '와아아~'];
+
 const BottomSideMenu = () => {
     const { inventoryToItems } = useSelector((state: RootState) => {
         return state.user.data.inventory;
@@ -56,6 +61,10 @@ const BottomSideMenu = () => {
     const { magicPowderGauge } = useSelector((state: RootState) => {
         return state.user.data.fairy;
     });
+
+    const { speechEvent: heartEvent } = useSpeech(heartSpeech);
+    const { speechEvent: magicPowderEvent } = useSpeech(magicPowderSpeech);
+    const { speechEvent: dewEvent } = useSpeech(dewSpeech);
 
     const queryClient = useQueryClient();
 
@@ -67,11 +76,21 @@ const BottomSideMenu = () => {
 
     const onClickHandler = (itemType: string) => {
         mutation.mutate(itemType);
+        if (itemType === 'heart') {
+            return heartEvent();
+        } else if (itemType === 'magicPowder') {
+            return magicPowderEvent();
+        } else if (itemType === 'dew') {
+            return dewEvent();
+        }
     };
-    // console.log(inventoryToItems[].name)
+
     return (
         <BottomsideMenu>
-            <PlayPageBottomButton onClick={() => onClickHandler('heart')}>
+            <PlayPageBottomButton
+                onClick={() => onClickHandler('heart')}
+                disabled={inventoryToItems[2].quantity === 0}
+            >
                 <ButtonBox className="love">
                     <Heart />
                     사랑해주기 <br />
@@ -80,7 +99,7 @@ const BottomSideMenu = () => {
             </PlayPageBottomButton>
             <PlayPageBottomButton
                 onClick={() => onClickHandler('magicPowder')}
-                disabled={magicPowderGauge > 90}
+                disabled={magicPowderGauge > 90 || inventoryToItems[1].quantity === 0}
             >
                 <ButtonBox className="magicpowder">
                     <MagicPowder />
@@ -89,7 +108,10 @@ const BottomSideMenu = () => {
                     <span>{inventoryToItems[1].quantity}개</span>
                 </ButtonBox>
             </PlayPageBottomButton>
-            <PlayPageBottomButton onClick={() => onClickHandler('dew')}>
+            <PlayPageBottomButton
+                onClick={() => onClickHandler('dew')}
+                disabled={inventoryToItems[0].quantity === 0}
+            >
                 <ButtonBox className="water">
                     <Water />
                     이슬먹이기
