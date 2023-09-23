703365ab16968bcf5187440daf2b9d1639c78331
gunhoray <raylee9649@gmail.com>
2023-09-23 23:12:13 +0900

    feat: Shower Animation

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -10,33 +10,46 @@ import LeafyGrow from '../components/gamecharcter/LeafyGrow';
 import ShareLink from '../components/common/ShareLink';
 import SpeechBubble from '../components/game/gameplay/SpeechBubble';
 import LastLevelUp from '../components/game/LastLevelUp';
+import { useState } from "react";
+import Shower from "../components/game/gameplay/Shower";
+import { Water } from "../assets/icons/GameIcon";
 
 const TestPage = () => {
-    return (
-        <>
-            <div
-                style={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    gap: '16px',
-                }}
-            >
-                <AttendanceReward />
-                {/* <AccessThreeReward /> */}
-                <WaterReward />
-                <MagicpowderReward />
-                {/* <LevelUp /> */}
-                <LastLevelUp />
-                <Menu />
-                <ShareLink />
-            </div>
-            <SpeechBubble />
-            <Folwery />
-            <Green1 />
-            <FloweryGrow />
-            <LeafyGrow />
-        </>
-    );
+  const [showers, setShowers] = useState(false);
+  const ShowerHandler = () => {
+    setShowers(true);
+    setTimeout(() => {
+      setShowers(false);
+    }, 1000);
+  };
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "16px",
+        }}
+      >
+        <AttendanceReward />
+        {/* <AccessThreeReward /> */}
+        <WaterReward />
+        <MagicpowderReward />
+        {/* <LevelUp /> */}
+        <LastLevelUp />
+        <Menu />
+        <ShareLink />
+      </div>
+      <button onClick={ShowerHandler}>샤워</button>
+      {showers && <Shower symbol={<Water />} />}
+
+      <SpeechBubble />
+      <Folwery />
+      <Green1 />
+      <FloweryGrow />
+      <LeafyGrow />
+    </>
+  );
 };
 
 export default TestPage;
