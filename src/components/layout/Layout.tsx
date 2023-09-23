c8cf7f3ca6c26780d90c1cd0d131e6def5fa2682
gunhoray <raylee9649@gmail.com>
2023-09-23 21:02:30 +0900

    feat: FairyBackground

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,29 +1,41 @@
 import React, { ReactNode } from 'react';
 import styled from 'styled-components';
-
+import Fairybackground from "../../assets/Fairygamebackground.png";
 const LayoutStyle = styled.div`
-    max-width: 360px;
-    height: 100vh;
-    margin: auto;
+  max-width: 360px;
+  height: 100vh;
+  margin: auto;
+  background-color: white;
+  overflow: hidden;
+  overflow-y: auto;
+  position: relative;
 
-    overflow: hidden;
-    overflow-y: auto;
-    position: relative;
-    main {
-        /* padding: 0 0.8rem; */
-    }
-    &::-webkit-scrollbar {
-        display: none;
-    }
-    -ms-overflow-style: none;
-    scrollbar-width: none;
+  main {
+    /* padding: 0 0.8rem; */
+  }
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
+
+const FairyBackground = styled.div`
+  background-image: url(${Fairybackground});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 type LayoutProps = {
-    children: ReactNode;
+  children: ReactNode;
 };
 const Layout = ({ children }: LayoutProps) => {
-    return <LayoutStyle>{children}</LayoutStyle>;
+  return (
+    <FairyBackground>
+      <LayoutStyle>{children}</LayoutStyle>
+    </FairyBackground>
+  );
 };
 
 export default Layout;
