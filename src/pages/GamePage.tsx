dc12e736981fd863b4d14515079e26c0a06e9442
Haegnim <haegnim@gmail.com>
2023-09-24 22:55:57 +0900

    Fix : error

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,40 +1,43 @@
-import React, { useEffect } from "react";
-import GameCreate from "../components/game/GameCreate";
-import GamePlay from "../components/game/gameplay/GamePlay";
-import { useDispatch, useSelector } from "react-redux";
-import { useQuery } from "react-query";
-import { fetchUser } from "../apis/AuthApi";
-import { fetchUserSuccess } from "../redux/modules/userSlice";
-import { RootState } from "../redux/config/configStore";
+import React, { useEffect } from 'react';
+import GameCreate from '../components/game/GameCreate';
+import GamePlay from '../components/game/gameplay/GamePlay';
+import { useDispatch, useSelector } from 'react-redux';
+import { useQuery } from 'react-query';
+import { fetchUser } from '../apis/AuthApi';
+import { fetchUserSuccess } from '../redux/modules/userSlice';
+import { RootState } from '../redux/config/configStore';
 const checkFairy = (fairy: string) => {
-  return fairy !== null;
+    return fairy !== null;
 };
 
 const GamePage = () => {
-  const dispatch = useDispatch();
-  const { data, isLoading, isError } = useQuery("user", fetchUser, {
-    onSuccess: (data: any) => {
-      dispatch(fetchUserSuccess(data));
-    },
-  });
+    const dispatch = useDispatch();
+    const { data, isLoading, isError } = useQuery('user', fetchUser, {
+        onSuccess: (data: any) => {
+            dispatch(fetchUserSuccess(data));
+        },
+        onError: (err) => {
+            alert('해당 환경은 지원하지않습니다.');
+        },
+    });
 
-  if (isLoading) {
-    return <span>Loading…</span>;
-  }
+    if (isLoading) {
+        return <span>Loading…</span>;
+    }
 
-  if (isError) {
-    return <span>Error</span>;
-  }
+    if (isError) {
+        return <span>Error</span>;
+    }
 
-  const hasFairy = checkFairy(data.fairy);
+    const hasFairy = checkFairy(data.fairy);
 
-  return <>{hasFairy ? <GamePlay /> : <GameCreate />}</>;
-  // return (
-  //   <>
-  //     <GamePlay />
-  //     {/* <GameCreate /> */}
-  //   </>
-  // );
+    return <>{hasFairy ? <GamePlay /> : <GameCreate />}</>;
+    // return (
+    //   <>
+    //     <GamePlay />
+    //     {/* <GameCreate /> */}
+    //   </>
+    // );
 };
 
 export default GamePage;
